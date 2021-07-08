89100c571c1a799299e1be80dbc0ba73030ee256
GarvG <59773926+toogiii@users.noreply.github.com>
2021-07-08 10:35:30 -0400

    Added 90 minute update

--- Musical Neural Network/playMusic.py
+++ Musical Neural Network/playMusic.py
@@ -3,19 +3,19 @@ import pygame
 def play_music(file):
     clock = pygame.time.Clock()
     try:
-        pygame.mixer.music.load(music_file)
+        pygame.mixer.music.load(file)
     except pygame.error:
         return
     pygame.mixer.music.play()
     while pygame.mixer.music.get_busy():
         clock.tick(30)
         
-midi = "60min.mid"
+midi_file = r"Evolution of Music/60min.mid"
 freq = 44100
 bitsize = -16
 channels = 2
 buffer = 1024
-pygame.mixer.init(freq, bitsize, channes, buffer)
+pygame.mixer.init(freq, bitsize, channels, buffer)
 
 pygame.mixer.music.set_volume(1.0)
 try:
