4296273f8980299789397837a8d1db2bc8a80fc0
GarvG <59773926+toogiii@users.noreply.github.com>
2021-07-08 10:13:26 -0400

    Evolution Folders, midiPlay function

---
+++ Musical Neural Network/playMusic.py
@@ -0,0 +1,26 @@
+import pygame
+
+def play_music(file):
+    clock = pygame.time.Clock()
+    try:
+        pygame.mixer.music.load(music_file)
+    except pygame.error:
+        return
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        clock.tick(30)
+        
+midi = "60min.mid"
+freq = 44100
+bitsize = -16
+channels = 2
+buffer = 1024
+pygame.mixer.init(freq, bitsize, channes, buffer)
+
+pygame.mixer.music.set_volume(1.0)
+try:
+    play_music(midi_file)
+except KeyboardInterrupt:
+    pygame.mixer.music.fadeout(1000)
+    pygame.mixer.music.stop()
+    raise SystemExit
